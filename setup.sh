80979097f877a3ebf5b7e41df22feed8fa6c0f0c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-11-30 10:30:00 +1000

    feat: add redis docker doco

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-docker run --name ftc-redis -d redis
+docker run --name redis-vecdb -d -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
